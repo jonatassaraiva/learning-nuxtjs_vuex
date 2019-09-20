85fe16e449f27cd0895bca57cbf6b12016ce4744
Jonatas Saraiva <jonatas.saraiva@gmail.com>
2019-09-20 09:16:49 -0300

    Layout e pages

---
+++ components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="nav" class="nav">
+    <nuxt-link class="brand" to="/">Real World Events</nuxt-link>
+    <nuxt-link class="nav-item" to="/list">List of events</nuxt-link>
+    <nuxt-link class="nav-item" to="/create">Create Events</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+}
+.nav > .brand {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
+  font-size: 1.5em;
+  color: #39b982;
+  text-decoration: none;
+}
+.nav .nav-item {
+  box-sizing: border-box;
+  margin: 0 5px;
+  color: rgba(0, 0, 0, 0.5);
+  text-decoration: none;
+}
+.nav .nav-item.nuxt-link-exact-active {
+  color: #39b982;
+  border-bottom: solid 2px #39b982;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,24 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        learning-nuxtjs_vuex
-      </h1>
-      <h2 class="subtitle">
-        My bee&#39;s knees Nuxt.js project
-      </h2>
+      <h1 class="title">learning-nuxtjs_vuex</h1>
+      <h2 class="subtitle">My bee&#39;s knees Nuxt.js project</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      text: 'test'
+    };
   }
-}
+};
 </script>
 
 <style>
